d29ba13d366a69993c5204aca125d94abb037e83
Albert <albert.palau@oracle.com>
2018-07-13 18:06:17 +0200

    added deposit function

--- plasma/contracts/rootChain.sol
+++ plasma/contracts/rootChain.sol
@@ -1,72 +1,79 @@
 pragma solidity ^0.4.23;
 
 contract rootChain {
-   address public operator;
-   uint256 public currentChildBlock;
-   mapping (uint256 => ChildBlock) public childChain;
+    address public operator;
+    uint256 public currentChildBlock;
+    mapping (uint256 => ChildBlock) public childChain;
 
 
-   event BlockSubmitted(
+    event BlockSubmitted(
         bytes32 root,
         uint256 timestamp
-   );
+    );
 
-   struct ChildBlock {
-       bytes32 root;
-       uint256 timestamp;
-   }
-
-   modifier onlyOperator() {
-       require(msg.sender == operator);
-       _;
-   }
-
-   constructor()
-       public
-   {
-       operator = msg.sender;
-       currentChildBlock = 0;
-   }
-
-   function submitBlock(bytes32 _root)
-       public
-       onlyOperator
-   {
-       childChain[currentChildBlock] = ChildBlock({
-           root: _root,
-           timestamp: block.timestamp
-       });
-
-       // Update block numbers.
-       currentChildBlock = currentChildBlock + 1;
-
-       emit BlockSubmitted(_root, block.timestamp);
-   }
-
-   function getChildChain(uint256 _blockNumber)
-    public
-    view
-    returns (bytes32, uint256)
-    {
-      return (childChain[_blockNumber].root, childChain[_blockNumber].timestamp);
+    struct ChildBlock {
+        bytes32 root;
+        uint256 timestamp;
     }
 
-   // TODO
-   function deposit()
-    public
-    payable
+    event Deposit(
+        address indexed depositor,
+        uint256 indexed depositBlock,
+        address token,
+        uint256 amount
+    );
+    modifier onlyOperator() {
+        require(msg.sender == operator);
+        _;
+    }
+
+    constructor()
+       public
     {
-      // Only allow up to CHILD_BLOCK_INTERVAL deposits per child block.
-      require(currentDepositBlock < CHILD_BLOCK_INTERVAL);
+        operator = msg.sender;
+        currentChildBlock = 0;
+    }
 
-      bytes32 root = keccak256(msg.sender, address(0), msg.value);
-      uint256 depositBlock = getDepositBlock();
-      childChain[depositBlock] = ChildBlock({
-          root: root,
-          timestamp: block.timestamp
-      });
-      currentDepositBlock = currentDepositBlock.add(1);
+    function submitBlock(bytes32 _root)
+       public
+       onlyOperator
+    {
+        childChain[currentChildBlock] = ChildBlock({
+            root: _root,
+            timestamp: block.timestamp
+        });
 
-      emit Deposit(msg.sender, depositBlock, address(0), msg.value);
+       // Update block numbers.
+        currentChildBlock = currentChildBlock + 1;
+
+        emit BlockSubmitted(_root, block.timestamp);
+    }
+
+    function getChildChain(uint256 _blockNumber)
+        public
+        view
+        returns (bytes32, uint256)
+    {
+        return (childChain[_blockNumber].root, childChain[_blockNumber].timestamp);
+    }
+
+    // TODO
+    function deposit()
+        public
+        payable
+    {
+        // Only allow up to CHILD_BLOCK_INTERVAL deposits per child block.
+        //require(currentDepositBlock < CHILD_BLOCK_INTERVAL);
+
+        bytes32 root = keccak256(msg.sender, address(0), msg.value);
+ 
+        uint depositBlock = currentChildBlock + 1;
+        childChain[depositBlock] = ChildBlock({
+            root: root,
+            timestamp: block.timestamp
+        });
+        currentChildBlock = depositBlock;
+
+        emit Deposit(msg.sender, depositBlock, address(0), msg.value);
     }
 }
--- plasma/test/rootChain.js
+++ plasma/test/rootChain.js
@@ -15,4 +15,13 @@ contract('rootChain', function(accounts) {
        assert.equal(last_block[0], "0x8e61fce813b2909e7e4b534e0b77ad99c56e6b411b28559f6a3ce4065a50cc7d");
     });
   });
+  it("Deposit", function() {
+    var rc;
+    return rootChain.deployed().then(function(instance) {
+      rc = instance;
+      return rc.deposit({value:1})
+    }).then(function() {
+       assert.equal(true, true)
+    });
+  });
 });
